0e2d6cc6aa8daea2b446385e443d038376737e26
Yongcheng Wu <yc_wu1030@126.com>
2021-03-15 23:46:36 -0500

    fix(HermiteInterpolation): New method in HermiteInterpolation

--- include/HermiteInterpolation.h
+++ include/HermiteInterpolation.h
@@ -14,13 +14,37 @@ private:
     VD dF;// Dimension NDim
     int NDim;
 
-    double* Z; //Dimension 2NDim
-    double** Q; // Dimension (2NDim) x (2NDim)
+    // double* Z; //Dimension 2NDim
+    // double** Q; // Dimension (2NDim) x (2NDim)
+    inline double H00(double t);
+    inline double H10(double t);
+    inline double H01(double t);
+    inline double H11(double t);
+
+    int Find_Index(double x); // Find the index i, such that  X[i]<= x < X[i+1], using binary search.
 public:
     HermiteInterpolation1D(VD _X, VD _F, VD _dF);
     ~HermiteInterpolation1D();
     double valAt(double x);
 };
 
+double HermiteInterpolation1D::H00(double t)
+{
+    return (1+2*t)*(1-t)*(1-t);
+}
 
+double HermiteInterpolation1D::H10(double t)
+{
+    return t*(1-t)*(1-t);
+}
+
+double HermiteInterpolation1D::H01(double t)
+{
+    return t*t*(3-2*t);
+}
+
+double HermiteInterpolation1D::H11(double t)
+{
+    return t*t*(t-1);
+}
 #endif
--- src/HermiteInterpolation.cpp
+++ src/HermiteInterpolation.cpp
@@ -2,62 +2,112 @@
  * @Description  : Hermite Interpolation
  */
 #include "HermiteInterpolation.h"
+#include <iostream>
 using namespace std;
 // * Using https://www3.nd.edu/~zxu2/acms40390hw/Hermite.cpp Failed
 // * Good for NDim is not that large.
 // * Has problem for NDim is large.
+// * So, I use the method described in Wiki: https://en.wikipedia.org/wiki/Cubic_Hermite_spline
 HermiteInterpolation1D::HermiteInterpolation1D(VD _X, VD _F, VD _dF)
 {
     X = _X;
     F = _F;
     dF = _dF;
     NDim = X.size();
-    Z = new double[2*NDim];
-    Q = new double*[2*NDim];
-    for (size_t i = 0; i < NDim; i++)
-    {
-        Q[2*i] = new double[2*NDim];
-        Q[2*i+1] = new double[2*NDim];
-        Z[2*i] = X[i];
-        Z[2*i+1] = X[i];
-        Q[2*i][0] = F[i];
-        Q[2*i+1][0] = F[i];
-        Q[2*i+1][1] = dF[i];
-        if (i!=0)
-        {
-            Q[2*i][1] = (Q[2*i][0]-Q[2*i-1][0])/(Z[2*i]-Z[2*i-1]);
-        } 
-    }
-    int K = 2*(NDim-1) + 1;
-    for (size_t i = 2; i <= K; i++)
-    {
-        for (size_t j = 2; j <= i; j++)
-        {
-            Q[i][j] = (Q[i][j-1]-Q[i-1][j-1])/(Z[i]-Z[i-j]);
-        }
-    }
+    // Z = new double[2*NDim];
+    // Q = new double*[2*NDim];
+    // for (size_t i = 0; i < NDim; i++)
+    // {
+    //     Q[2*i] = new double[2*NDim];
+    //     Q[2*i+1] = new double[2*NDim];
+    //     Z[2*i] = X[i];
+    //     Z[2*i+1] = X[i];
+    //     Q[2*i][0] = F[i];
+    //     Q[2*i+1][0] = F[i];
+    //     Q[2*i+1][1] = dF[i];
+    //     if (i!=0)
+    //     {
+    //         Q[2*i][1] = (Q[2*i][0]-Q[2*i-1][0])/(Z[2*i]-Z[2*i-1]);
+    //     } 
+    // }
+    // int K = 2*(NDim-1) + 1;
+    // for (size_t i = 2; i <= K; i++)
+    // {
+    //     for (size_t j = 2; j <= i; j++)
+    //     {
+    //         Q[i][j] = (Q[i][j-1]-Q[i-1][j-1])/(Z[i]-Z[i-j]);
+    //     }
+    // }
 }
 
+int HermiteInterpolation1D::Find_Index(double x)
+{
+    // Find the index i, such that  X[i]<= x < X[i+1], using binary search.
+    // if x < X[0] return -1
+    // if x > X[-1] return NDim
+    if (x<X[0])
+    {
+        return -1;
+    }
+    if (x>X[NDim-1])
+    {
+        return NDim;
+    }
+    if (x==X[NDim-1])
+    {
+        return NDim-2;
+    }
+    int indmin = 0;
+    int indmax = NDim-2;
+    int ind = (indmax+indmin)/2;
+    while (true)
+    {
+        // cout<<x<<" ["<<X[ind]<<","<<X[ind+1]<<"]"<<endl;
+        if (x<X[ind])
+        {
+            indmax = ind;
+            ind = (indmax+indmin)/2;
+            continue;
+        }
+        if (x>=X[ind+1])
+        {
+            indmin = ind+1;
+            ind = (indmax+indmin)/2;
+            continue;
+        }
+        break;
+    }
+    return ind;
+}
 HermiteInterpolation1D::~HermiteInterpolation1D()
 {
-    delete[] Z;
-    for (size_t i = 0; i < 2*NDim; i++)
-    {
-        delete[] Q[i];
-    }
-    delete[] Q;
+    // delete[] Z;
+    // for (size_t i = 0; i < 2*NDim; i++)
+    // {
+    //     delete[] Q[i];
+    // }
+    // delete[] Q;
 }
 
 double HermiteInterpolation1D::valAt(double x)
 {
-    int K = 2*NDim-1;
-    int J;
-    double S = Q[K][K]*(x-Z[K-1]);
-    for (size_t i = 2; i <= K; i++)
+    // int K = 2*NDim-1;
+    // int J;
+    // double S = Q[K][K]*(x-Z[K-1]);
+    // for (size_t i = 2; i <= K; i++)
+    // {
+    //     J = K - i + 1;
+    //     S = (S+Q[J][J])*(x-Z[J-1]);
+    // }
+    // S += Q[0][0];
+    // return S;
+    int ind = Find_Index(x);
+    if (ind < 0 || ind == NDim)
     {
-        J = K - i + 1;
-        S = (S+Q[J][J])*(x-Z[J-1]);
+        return 0;
     }
-    S += Q[0][0];
-    return S;
+    double xmax = X[ind+1];
+    double xmin = X[ind];
+    double t = (x-xmin)/(xmax-xmin);
+    return H00(t)*F[ind]+H10(t)*(xmax-xmin)*dF[ind]+H01(t)*F[ind+1]+H11(t)*(xmax-xmin)*dF[ind+1];
 }
